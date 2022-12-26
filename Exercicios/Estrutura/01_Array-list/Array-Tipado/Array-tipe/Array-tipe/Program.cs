6b3059c0e22a6a6483a9355181d129338cf56fc0
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2022-12-26 17:31:36 -0300

    Arrays-Complete

---
+++ Exercicios/Estrutura/01_Array-list/Array-Tipado/Array-tipe/Array-tipe/Program.cs
@@ -0,0 +1,19 @@
+﻿var arrayTiponumero = new int[3];
+arrayTiponumero[0] = 10;
+arrayTiponumero[1] = 15;
+arrayTiponumero[2] = 20;
+
+Array.Resize(ref arrayTiponumero, 100);
+arrayTiponumero[10] = 200;
+
+foreach(var item in arrayTiponumero)
+{
+    Console.WriteLine(item);
+}
+
+var arrayTostring = new string[2] {"Pablo","Fabricio"};
+
+foreach(var item in arrayTostring)
+{
+    Console.WriteLine(item);
+}
---
+++ Exercicios/Estrutura/01_Array-list/Array-list/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+var arraylist = new ArrayList();
+
+arraylist.Add(1);
+arraylist.Add("Rafael");
+arraylist.Add(true);
+
+Console.WriteLine(arraylist[2]);
+
+//arraylist.RemoveAt(1); remove o item
+//arraylist.Clear(); Limpa todo o array
+
+foreach(var item in arraylist)
+{
+    Console.WriteLine(item);
+}
