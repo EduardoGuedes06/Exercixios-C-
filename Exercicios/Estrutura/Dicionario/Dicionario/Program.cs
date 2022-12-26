646740e04402cd6ea5058a1fdcfc1dfa4efc6193
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2022-12-26 17:55:42 -0300

    Gato-Fodido deu push

---
+++ Exercicios/Estrutura/Dicionario/Dicionario/Program.cs
@@ -0,0 +1,22 @@
+﻿var Dicionario = new Dictionary<int,string>();
+Dicionario.Add(100,"Rafael");
+Dicionario[50]="Curso";
+var nome = Dicionario[50];
+
+Console.WriteLine(nome);
+
+var Pablo = new Dictionary<string,string>()
+{
+    {"Teste 1 de 1","Teste 2 de 1"},
+    {"Teste 1 de 2","Teste 2 de 2"},
+};
+
+foreach( var item in Dicionario)
+{
+    Console.WriteLine(item);
+}
+
+foreach( var item in Pablo)
+{
+    Console.WriteLine(item.Key);
+}
---
+++ Exercicios/Estrutura/Lista/Lista/Program.cs
@@ -0,0 +1,14 @@
+﻿var lista = new List<string>(10);
+
+lista.Add("Rafael");
+lista.Add("Pedro");
+lista.Add("Antony");
+
+var nome = lista[0];
+lista.RemoveAt(0);
+Console.WriteLine(nome);
+
+foreach(var item in lista)
+{
+    Console.WriteLine(item);
+}
