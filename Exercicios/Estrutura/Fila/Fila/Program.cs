e7d077eccd7e30a74ee74fe3e3a3b121b9af21f3
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2022-12-27 13:42:33 -0300

    fila-pilha

---
+++ Exercicios/Estrutura/Fila/Fila/Program.cs
@@ -0,0 +1,9 @@
+﻿var queue = new Queue<string>();
+
+queue.Enqueue("Eduardo");
+queue.Enqueue("Rafael");
+
+var nome = queue.Dequeue();
+var nome_1 = queue.Dequeue();
+Console.WriteLine(nome);
+Console.WriteLine(nome_1);
---
+++ Exercicios/Estrutura/Pilha/Pilha/Program.cs
@@ -0,0 +1,17 @@
+﻿var stack = new Stack<string>();
+
+stack.Push("Eduardo");
+stack.Push("Pablo");
+
+
+var nome = stack.Pop();
+var nome_1 = stack.Pop();
+
+Console.WriteLine(nome);
+Console.WriteLine(nome_1);
+
+
+foreach( var item in stack)
+{
+    Console.WriteLine(item);
+}
