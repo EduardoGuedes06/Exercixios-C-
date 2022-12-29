c9d6ddb7a507581d8fe0089a7c81ae6cb9efb66c
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2022-12-29 16:08:41 -0300

    Arquivos - Finish

---
+++ Exercicios/Estruturas/Arquivos/Arquivo/Code.cs
@@ -0,0 +1,35 @@
+﻿namespace Arquivos;
+public class ArquivosClass
+{
+    public void CriandoArquivo()
+    {
+        var escrever = new StreamWriter("Cadastro.txt", true);
+        Console.Write("Informe um nome: ");
+        var nome = Console.ReadLine();
+        escrever.WriteLine("ID...: " + Random.Shared.Next(1, 100));
+        escrever.WriteLine("Nome.: " + nome);
+        escrever.WriteLine("----------------------");
+        escrever.Close();
+    }
+
+    public void LendoArquivo()
+    {
+
+        var ler = new StreamReader("Cadastro.txt");
+        while(!ler.EndOfStream)
+        {
+            var linha = ler.ReadLine();
+            Console.WriteLine(linha);
+        }
+
+        ler.Close();
+    }
+
+    public void ExcluindoArquivo()
+    {
+        if(File.Exists("Cadastro.txt"))
+        {
+            File.Delete("Cadastro.txt");
+        }
+    }
+}
---
+++ Exercicios/Estruturas/Arquivos/Arquivo/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+            
+    }
+
+             private static void TrabalhandoComArquivos()
+         {
+                var trabalhandoComArquivos = new Arquivos.ArquivosClass();
+                trabalhandoComArquivos.CriandoArquivo();
+                //trabalhandoComArquivos.LendoArquivo();
+                //trabalhandoComArquivos.ExcluindoArquivo();
+         }
+}
